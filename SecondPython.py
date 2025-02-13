c375b607ae2f8dc06b9bdd90aebd2391cee543e2
Bills <schweindb@hotmail.com>
2025-02-12 20:51:21 -0600

    Changed Second

--- SecondPython.py
+++ SecondPython.py
@@ -1,3 +1,12 @@
 Cars = ["Mazda", "Ford", "Volkswagen"]
 for x in Cars:
   print(x)
+  #section two
+myfile = open('C:\ccc\dataone.csv')
+print(myfile.read())
+#section whatever
+
+x = "Hello jfjfjfjffj"
+print(x)
+x = 55
+print(x)
