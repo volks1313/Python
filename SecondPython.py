f25fba3e440e8ffd2824aae7381b3d77dca94565
Bills <schweindb@hotmail.com>
2025-02-12 21:13:47 -0600

    Second testing

--- SecondPython.py
+++ SecondPython.py
@@ -1,12 +1,25 @@
-Cars = ["Mazda", "Ford", "Volkswagen"]
-for x in Cars:
-  print(x)
-  #section two
-myfile = open('C:\ccc\dataone.csv')
-print(myfile.read())
+# Cars = ["Mazda", "Ford", "Volkswagen"]
+# for x in Cars:
+#   print(x)
+#   #section two
+# myfile = open('C:\ccc\dataone.csv')
+# print(myfile.read())
 #section whatever
+import os
+clear = lambda: os.system('cls')
+clear()
 
-x = "Hello jfjfjfjffj"
-print(x)
-x = 55
-print(x)
+
+# x = "Hello jfjfjfjffj"
+# print(x)
+# print(type(x))
+# x = 55
+# print(type(x))
+# x = [1,2,3,4,5,7]
+# print(type(x))
+# for y in x:
+#     print(y)
+# x = (1,2,3,4,5,7,9,11)
+# print(type(x))
+# for y in x:
+    # print(y)    
